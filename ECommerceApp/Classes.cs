617ec37530befb49ebc992b22faa9a4617627779
jespernapp <jesperooo@live.se>
2025-09-08 20:01:36 +0200

    Add project files.

---
+++ ECommerceApp/Classes.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceApp
+{
+    internal class Classes
+    {
+        static void CreateCustomer(string name, string email, string connectionString)
+        {
+            using (var conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                string sql = "INSERT INTO Customers (Name, Email) VALUES (@Name, @Email)";
+                using (var cmd = new SqlCommand(sql, conn))
+                {
+                    cmd.Parameters.AddWithValue("@Name", name);
+                    cmd.Parameters.AddWithValue("@Email", email);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+        static int CreateOrder (int customerId, string connectionString)
+        {
+            using (var conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                string sql = "INSERT INTO ORDERS (CustomerId, OrderDate) OUTPUT INSERTED.OrderId VALUES (@CustomerId, @OrderDate)";
+                using (var cmd = new SqlCommand(sql, conn))
+                {
+                    cmd.Parameters.AddWithValue("@CustomerId", customerId);
+                    cmd.Parameters.AddWithValue("@OrderDate", DateTime.Now);
+                    return (int)cmd.ExecuteScalar();    
+                }
+            }
+        }
+    }
+}
---
+++ ECommerceApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceApp
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            // Connection string for local SQL Server database
+            string connectionString = @"Server=(localdb)\mssqllocaldb;Database=ECommerceAppDb;Trusted_Connection=True;MultipleActiveResultSets=true";
+        }
+    }
+
+}
